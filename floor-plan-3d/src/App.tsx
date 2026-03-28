6b436dd6bd9c7cb3eefaace2222620a82e96e942
SONRA <sobhanranjbar2@gmail.com>
2026-03-27 22:48:06 -0400

    scaffold floor-plan-3d project with Vite + React + TypeScript

---
+++ floor-plan-3d/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>3D Floor Plan Visualizer</h1>
+      <p>Upload your floor plan to get started.</p>
+    </div>
+  );
+};
+
+export default App;
